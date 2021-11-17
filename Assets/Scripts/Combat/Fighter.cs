bfc24d27e8e601ba5ff88a60db1933cdf924e92d
tumatihub <jgsoaresdias@gmail.com>
2021-11-17 18:39:28 -0300

    4.36 - Taking Damage

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -12,6 +12,7 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
         float timeSinceLastAttack = 0;
@@ -72,7 +73,7 @@ namespace RPG.Combat
         // Animation Event
         void Hit()
         {
-
+            target.GetComponent<Health>()?.TakeDamage(weaponDamage);
         }
     }
 }
---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float health = 100f;
+
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(0, health - damage);
+            print(health);
+        }
+    }
+}
