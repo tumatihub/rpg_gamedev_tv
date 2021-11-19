b44a43d77e30ff6732ba61a4196ef74bd909ddc2
tumatihub <jgsoaresdias@gmail.com>
2021-11-19 11:20:51 -0300

    5.52 - Visualising Waypoints

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointGizmoRadius = 0.3f;
+
+        void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.DrawSphere(transform.GetChild(i).transform.position, waypointGizmoRadius);
+            }
+        }
+    }
+}
