f8d59a6ecc32f992126bec781ee672e73aa66a52
tumatihub <jgsoaresdias@gmail.com>
2021-11-24 17:22:05 -0300

    7.70 - Scene Loading Portals

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneIndex = -1;
+        
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+    }
+}
