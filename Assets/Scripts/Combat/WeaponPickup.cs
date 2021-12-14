8752860522e4a9cb66333bacba826d256092bc19
tumatihub <jgsoaresdias@gmail.com>
2021-12-14 17:46:01 -0300

    10.128 - Pickup Respawing

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Combat
 {
+    [RequireComponent(typeof(Collider))]
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
 
         void OnTriggerEnter(Collider other)
         {
@@ -14,8 +17,33 @@ namespace RPG.Combat
             {
                 Fighter fighter = other.GetComponent<Fighter>();
                 fighter.EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
         }
+
+        IEnumerator HideForSeconds(float seconds)
+        {
+            HidePickup();
+            yield return new WaitForSeconds(seconds);
+            ShowPickup();
+        }
+        void HidePickup()
+        {
+            GetComponent<Collider>().enabled = false;
+            foreach (Transform child in GetComponentInChildren<Transform>())
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        void ShowPickup()
+        {
+            GetComponent<Collider>().enabled = true;
+            foreach (Transform child in GetComponentInChildren<Transform>())
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+
     }
 }
