ab9de6ba31be4c726e6637988172ba19681ba652
tumatihub <jgsoaresdias@gmail.com>
2021-12-19 17:17:00 -0300

    12.166 - Updating The Damage Text Value

--- Assets/Scripts/UI/DamageText/DamageText.cs
+++ Assets/Scripts/UI/DamageText/DamageText.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RPG.UI.DamageText
 {
     public class DamageText : MonoBehaviour
     {
+        [SerializeField] Text textDisplay;
+
+        public void SetDamageValue(float damage)
+        {
+            textDisplay.text = $"{damage:0}";
+        }
     }
 }
