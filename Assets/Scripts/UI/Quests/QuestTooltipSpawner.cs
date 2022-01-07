efd8a1717b31e40c4900337d12b65620c315e2ec
tumatihub <jgsoaresdias@gmail.com>
2022-01-07 18:48:40 -0300

    5.53 - Tooltip Spawning & Show Hide

---
+++ Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,19 @@
+using GameDevTV.Core.UI.Tooltips;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.Quests
+{
+    public class QuestTooltipSpawner : TooltipSpawner
+    {
+        public override bool CanCreateTooltip()
+        {
+            return true;
+        }
+
+        public override void UpdateTooltip(GameObject tooltip)
+        {
+        }
+    }
+}
