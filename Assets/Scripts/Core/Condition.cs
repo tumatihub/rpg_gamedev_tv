2e976cc95f20d9b11e54a2d6b0453d8a9d8d3601
tumatihub <jgsoaresdias@gmail.com>
2022-01-09 18:25:35 -0300

    5.64 - Complex Conditions

--- Assets/Scripts/Core/Condition.cs
+++ Assets/Scripts/Core/Condition.cs
@@ -7,19 +7,50 @@ namespace RPG.Core
     [System.Serializable]
     public class Condition
     {
-        [SerializeField] string predicate;
-        [SerializeField] string[] parameters;
+        [SerializeField] Disjunction[] and;
 
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
         {
-            foreach (var evaluator in evaluators)
+            foreach (Disjunction disjunction in and)
             {
-                bool? result = evaluator.Evaluate(predicate, parameters);
-                if (result == null) continue;
-
-                if (result == false) return false;
+                if (!disjunction.Check(evaluators)) return false;
             }
             return true;
+        }
+
+        [System.Serializable]
+        class Disjunction
+        {
+            [SerializeField] Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (Predicate predicate in or)
+                {
+                    if (predicate.Check(evaluators)) return true;
+                }
+                return false;
+            }
+        }
+
+        [System.Serializable]
+        class Predicate
+        {
+            [SerializeField] string predicate;
+            [SerializeField] string[] parameters;
+            [SerializeField] bool not;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (var evaluator in evaluators)
+                {
+                    bool? result = evaluator.Evaluate(predicate, parameters);
+                    if (result == null) continue;
+
+                    if (result == false) return not;
+                }
+                return !not;
+            }
         }
     }
 }
