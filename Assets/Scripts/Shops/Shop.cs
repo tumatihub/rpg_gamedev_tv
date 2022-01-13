7d1bd1bf529614d0541f6ffa0e213525185474cc
tumatihub <jgsoaresdias@gmail.com>
2022-01-13 18:53:31 -0300

    1.19 - Tracking Stock

--- Assets/Scripts/Shops/Shop.cs
+++ Assets/Scripts/Shops/Shop.cs
@@ -23,11 +23,20 @@ namespace RPG.Shops
         [SerializeField] StockItemConfig[] stockConfig;
 
         Dictionary<InventoryItem, int> transaction = new Dictionary<InventoryItem, int>();
+        Dictionary<InventoryItem, int> stock = new Dictionary<InventoryItem, int>();
         Shopper currentShopper;
 
         public string ShopName => shopName;
 
         public event Action OnChange;
+
+        void Awake()
+        {
+            foreach (StockItemConfig config in stockConfig)
+            {
+                stock[config.item] = config.initialStock;
+            }
+        }
 
         public void SetShopper(Shopper shopper)
         {
@@ -46,7 +55,7 @@ namespace RPG.Shops
                 float price = config.item.GetPrice() * (1 - config.buyingDiscountPercentage / 100);
                 int quantityInTransaction = 0;
                 transaction.TryGetValue(config.item, out quantityInTransaction);
-                yield return new ShopItem(config.item, config.initialStock, price, quantityInTransaction);
+                yield return new ShopItem(config.item, stock[config.item], price, quantityInTransaction);
             }
         }
 
@@ -75,10 +84,13 @@ namespace RPG.Shops
                     if (success)
                     {
                         AddToTransaction(item, -1);
+                        stock[item]--;
                         purse.UpdateBalance(-price);
                     }
                 }
             }
+
+            OnChange?.Invoke();
         }
 
         public float TransactionTotal()
@@ -97,7 +109,15 @@ namespace RPG.Shops
             {
                 transaction[item] = 0;
             }
-            transaction[item] += quantity;
+
+            if (transaction[item] + quantity > stock[item])
+            {
+                transaction[item] = stock[item];
+            }
+            else
+            {
+                transaction[item] += quantity;
+            }
 
             if (transaction[item] <= 0)
             {
