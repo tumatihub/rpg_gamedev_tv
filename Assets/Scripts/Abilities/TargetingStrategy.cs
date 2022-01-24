b03f610a2ea188ccc5aec7dabc9c7c61bdf56604
tumatihub <jgsoaresdias@gmail.com>
2022-01-24 18:06:26 -0300

    2.36 - Overriding And Abstract Classes

---
+++ Assets/Scripts/Abilities/TargetingStrategy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.Abilities
+{
+    public abstract class TargetingStrategy : ScriptableObject
+    {
+        public abstract void StartTargeting();
+    }
+}
