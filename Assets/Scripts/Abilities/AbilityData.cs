286a552114f52be526e47053e075a7aa4e947960
tumatihub <jgsoaresdias@gmail.com>
2022-01-25 19:02:55 -0300

    2.43 - Blast Particle Effects

--- Assets/Scripts/Abilities/AbilityData.cs
+++ Assets/Scripts/Abilities/AbilityData.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 using System;
 
 namespace RPG.Abilities
@@ -7,6 +8,7 @@ namespace RPG.Abilities
     public class AbilityData
     {
         GameObject user;
+        Vector3 targetedPoint;
         IEnumerable<GameObject> targets;
 
         public AbilityData(GameObject user)
@@ -24,9 +26,24 @@ namespace RPG.Abilities
             this.targets = targets;
         }
 
+        public void SetTargetedPoint(Vector3 targetedPoint)
+        {
+            this.targetedPoint = targetedPoint;
+        }
+
+        public Vector3 GetTargetedPoint()
+        {
+            return targetedPoint;
+        }
+
         public GameObject GetUser()
         {
             return user;
+        }
+
+        public void StartCoroutine(IEnumerator coroutine)
+        {
+            user.GetComponent<MonoBehaviour>().StartCoroutine(coroutine);
         }
     }
 }
---
+++ Assets/Scripts/Abilities/Effects/SpawnTargetPrefabEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace RPG.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Spawn Target Prefab Effect", menuName = "RPG/Abilities/Effects/Spawn Target Prefab")]
+    public class SpawnTargetPrefabEffect : EffectStrategy
+    {
+        [SerializeField] Transform prefabToSpawn;
+        [SerializeField] float destroyDelay = -1f;
+
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            data.StartCoroutine(Effect(data, finished));
+        }
+
+        IEnumerator Effect(AbilityData data, Action finished)
+        {
+            Transform instance = Instantiate(prefabToSpawn, data.GetTargetedPoint(), Quaternion.identity);
+            if (destroyDelay > 0)
+            {
+                yield return new WaitForSeconds(destroyDelay);
+                Destroy(instance.gameObject);
+            }
+            finished();
+        }
+    }
+}
