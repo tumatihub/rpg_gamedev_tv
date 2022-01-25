e816973ba86803519b7c104b921841d70e525b41
tumatihub <jgsoaresdias@gmail.com>
2022-01-25 19:26:24 -0300

    2.44 - Cooldown Timers

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -11,9 +11,16 @@ namespace RPG.Abilities
         [SerializeField] TargetingStrategy targetingStrategy;
         [SerializeField] FilterStrategy[] filterStrategies;
         [SerializeField] EffectStrategy[] effectStrategies;
+        [SerializeField] float cooldownTime = 0f;
 
         public override void Use(GameObject user)
         {
+            CooldownStore cooldownStore = user.GetComponent<CooldownStore>();
+            if (cooldownStore.GetTimeRemaining(this) > 0)
+            {
+                return;
+            }
+
             AbilityData data = new AbilityData(user);
             targetingStrategy.StartTargeting(data, () =>
             {
@@ -23,6 +30,9 @@ namespace RPG.Abilities
 
         void TargetAquired(AbilityData data)
         {
+            CooldownStore cooldownStore = data.GetUser().GetComponent<CooldownStore>();
+            cooldownStore.StartCooldown(this, cooldownTime);
+
             foreach (var filterStrategy in filterStrategies)
             {
                 data.SetTargets(filterStrategy.Filter(data.GetTargets()));
