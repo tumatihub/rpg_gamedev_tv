2797abf157f8c405080ee6fee79ff538efabb870
tumatihub <jgsoaresdias@gmail.com>
2022-01-26 14:44:04 -0300

    2.47 - Mana Regeneration

--- Assets/Scripts/Attributes/Mana.cs
+++ Assets/Scripts/Attributes/Mana.cs
@@ -6,12 +6,25 @@ namespace RPG.Attributes
     public class Mana : MonoBehaviour
     {
         [SerializeField] float maxMana = 200f;
+        [SerializeField] float manaRegenRate = 2;
 
         float mana;
 
         void Awake()
         {
             mana = maxMana;
+        }
+
+        void Update()
+        {
+            if (mana < maxMana)
+            {
+                mana += manaRegenRate * Time.deltaTime;
+                if (mana > maxMana)
+                {
+                    mana = maxMana;
+                }
+            }
         }
 
         public float GetMana()
