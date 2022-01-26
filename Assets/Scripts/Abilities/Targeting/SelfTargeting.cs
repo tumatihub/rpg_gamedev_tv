6f213b6651d7fd62b1bb042f8e35fad20b90533f
tumatihub <jgsoaresdias@gmail.com>
2022-01-26 18:17:21 -0300

    2.53 - Self Targeting Strategy

---
+++ Assets/Scripts/Abilities/Targeting/SelfTargeting.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace RPG.Abilities.Targeting
+{
+    [CreateAssetMenu(fileName = "Self Targeting", menuName = "RPG/Abilities/Targeting/Self")]
+    public class SelfTargeting : TargetingStrategy
+    {
+        public override void StartTargeting(AbilityData data, Action finished)
+        {
+            data.SetTargets(new GameObject[] { data.GetUser() });
+            data.SetTargetedPoint(data.GetUser().transform.position);
+            finished();
+        }
+    }
+}
