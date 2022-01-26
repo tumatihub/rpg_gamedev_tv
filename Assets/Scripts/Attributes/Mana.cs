bfd30345222b848410c1b812ae23559b561bef97
tumatihub <jgsoaresdias@gmail.com>
2022-01-26 15:06:10 -0300

    2.48 - Mana Progression

--- Assets/Scripts/Attributes/Mana.cs
+++ Assets/Scripts/Attributes/Mana.cs
@@ -1,49 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using GameDevTV.Utils;
+using RPG.Stats;
 
 namespace RPG.Attributes
 {
+    [RequireComponent(typeof(BaseStats))]
     public class Mana : MonoBehaviour
     {
-        [SerializeField] float maxMana = 200f;
-        [SerializeField] float manaRegenRate = 2;
-
-        float mana;
+        LazyValue<float> mana;
 
         void Awake()
         {
-            mana = maxMana;
+            mana = new LazyValue<float>(GetMaxMana);
         }
 
         void Update()
         {
-            if (mana < maxMana)
+            if (mana.value < GetMaxMana())
             {
-                mana += manaRegenRate * Time.deltaTime;
-                if (mana > maxMana)
+                mana.value += GetRegenRate() * Time.deltaTime;
+                if (mana.value > GetMaxMana())
                 {
-                    mana = maxMana;
+                    mana.value = GetMaxMana();
                 }
             }
         }
 
         public float GetMana()
         {
-            return mana;
+            return mana.value;
         }
 
         public float GetMaxMana()
         {
-            return maxMana;
+            return GetComponent<BaseStats>().GetStat(Stat.Mana);
+        }
+
+        public float GetRegenRate()
+        {
+            return GetComponent<BaseStats>().GetStat(Stat.ManaRegenRate);
         }
 
         public bool UseMana(float manaToUse)
         {
-            if (manaToUse > mana)
+            if (manaToUse > mana.value)
             {
                 return false;
             }
-            mana -= manaToUse;
+            mana.value -= manaToUse;
             return true;
         }
     }
