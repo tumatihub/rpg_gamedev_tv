8d54d989c489880dd71f6bec36ff8058466ed97d
tumatihub <jgsoaresdias@gmail.com>
2022-01-27 11:11:46 -0300

    3.59 - Trait Row UI Scripts

---
+++ Assets/Scripts/UI/TraitRowUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+using UnityEngine.UI;
+using RPG.Stats;
+
+namespace RPG.UI
+{
+    public class TraitRowUI : MonoBehaviour
+    {
+        [SerializeField] Trait trait;
+        [SerializeField] TextMeshProUGUI valueText;
+        [SerializeField] Button minusButton;
+        [SerializeField] Button plusButton;
+
+        int value = 0;
+
+        void Start()
+        {
+            minusButton.onClick.AddListener(() => Allocate(-1));
+            plusButton.onClick.AddListener(() => Allocate(1));
+        }
+
+        void Update()
+        {
+            minusButton.interactable = value > 0;
+            
+            valueText.text = value.ToString();
+        }
+
+        public void Allocate(int points)
+        {
+            value += points;
+            if (value < 0)
+            {
+                value = 0;
+            }
+        }
+    }
+}
