d87af325ff3c5a4e02722a8aba5065d6b0ca4dd6
tumatihub <jgsoaresdias@gmail.com>
2022-01-28 10:59:44 -0300

    4.70 - Switching UI Screens

---
+++ Assets/Scripts/UI/UISwitcher.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class UISwitcher : MonoBehaviour
+    {
+        [SerializeField] GameObject entryPoint;
+
+        void Start()
+        {
+            SwitchTo(entryPoint);
+        }
+
+        public void SwitchTo(GameObject toDisplay)
+        {
+            if (toDisplay.transform.parent != transform) return;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(child.gameObject == toDisplay);
+            }
+        }
+    }
+}
