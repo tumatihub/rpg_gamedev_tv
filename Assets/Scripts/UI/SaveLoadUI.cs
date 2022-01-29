c47edb0d4501821623c9a349fac7633247cd1071
tumatihub <jgsoaresdias@gmail.com>
2022-01-29 14:32:35 -0300

    4.72 - Load Game UI

---
+++ Assets/Scripts/UI/SaveLoadUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RPG.UI
+{
+    public class SaveLoadUI : MonoBehaviour
+    {
+        [SerializeField] Transform contentRoot;
+        [SerializeField] GameObject buttonPrefab;
+
+        void OnEnable()
+        {
+            foreach (Transform child in contentRoot)
+            {
+                Destroy(child.gameObject);
+            }
+
+            Instantiate(buttonPrefab, contentRoot);
+            Instantiate(buttonPrefab, contentRoot);
+            Instantiate(buttonPrefab, contentRoot);
+            Instantiate(buttonPrefab, contentRoot);
+        }
+    }
+}
